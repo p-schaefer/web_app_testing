e034e486aac39c76da68b370d7525771b9783093
p-schaefer <patrick.schaefer@mail.utoronto.ca>
2017-03-21 21:01:10 -0400

    testing

--- ui.R
+++ ui.R
@@ -16,24 +16,38 @@ sidebar <- dashboardSidebar(
   sidebarMenu(
     menuItem("Introduction", tabName = "introduction", icon = icon("dashboard")),
     menuItem("Data Input", icon = icon("th"), tabName = "data_input",
-             menuSubItem("Raw Data upload",tabName = "rawdata_input",
-                         fileInput(inputId="inrawbioFile",multiple = FALSE, label = h4("Choose CSV file"),accept=c(".csv",".txt")),
-                         checkboxInput("metdata",label="Input data are metrics",value=F),
-                         radioButtons(inputId="rawFormat", label = h4("Input File Format"),choices = list("Long" ,"Wide" ),inline=T))
+             menuItem("Raw Data upload",tabName = "rawdatainput",icon=NULL,
+                         fileInput(inputId="inrawbioFile",multiple = FALSE, label = h5("Choose CSV file"),accept=c(".csv",".txt")),
+                         checkboxInput("metdata",label=h5("Input data are metrics"),value=F),
+                         radioButtons(inputId="rawFormat", label = h5("Input File Format"),choices = list("Long" ,"Wide" ),inline=T)
+                         ),
+             conditionalPanel(condition="input.rawFormat == 'Long'",
+               menuItem("Specify fields",tabName = "Specifyfields",icon=NULL)
+             )
+             
              )
   )
 )
 
 body <- dashboardBody(
-  tabItems(
     tabItem(tabName = "introduction",
             h2("Dashboard tab content")
     ),
-    
-    tabItem(tabName = "data_input",
-            h2("Widgets tab content")
+    tabItem(tabName = "rawdatainput",
+            fluidRow(
+              box(h2("plot"), width=12)
+              ),
+            fluidRow(
+              box(h2("Describe below"), width=12)
+            ),
+            
+            fluidRow(
+              box(h2("Site Identifiers"),width=3),
+              box(h2("Taxa Identifiers"),width=3),
+              box(h2("Habitat Descriptors"),width=3),
+              box(h2("Coordinates"),width=3)
+            )
     )
-  )
 )
 
 dashboardPage(
