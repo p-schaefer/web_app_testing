b80cc8052899029271a8ea71bfc331d77d9c3a2a
p-schaefer <patrick.schaefer@mail.utoronto.ca>
2017-04-03 08:24:56 -0400

    update to v0.1

--- server.R
+++ server.R
@@ -1,9 +1,14 @@
-library(shiny)
 library(BenthicAnalysistesting)
+library(shinyjs)
+library(shiny)
 library(shinydashboard)
 library(DT)
+library(leaflet)
+library(sp)
 
-shinyServer(function(input, output) {
+jsResetCode <- "shinyjs.reset = function() {history.go(0)}"
+
+shinyServer(function(input, output, session) {
 
   #########################################################
   #DATA INPUT
@@ -13,23 +18,39 @@ shinyServer(function(input, output) {
   #Raw Data Manipulation
   ########################################################
 
-  raw.bio.data<- reactive({#Raw data input
-    validate(
-      need(input$inrawbioFile != "", "Please upload a data file")
-    )
-    output<-read.csv(input$inrawbioFile$datapath,strip.white=TRUE, header=F)
-    output<-data.frame(apply(output,2,as.character))
-    output
+  raw.bio.data<- reactiveValues(data=NULL)
+  
+  observeEvent(input$inrawbioFile,{
+    #req(input$inrawbioFile)
+    raw.bio.data$data<-read.csv(input$inrawbioFile$datapath,strip.white=TRUE, header=F)
+    raw.bio.data$data<-data.frame(apply(raw.bio.data$data,2,as.character))
+  })
+  
+  observeEvent(input$clear_all,{
+    shinyjs::reset('inrawbioFile')
+    shinyjs::reset('rawFormat')
+    
+    raw.bio.data$data<-NULL
+    taxa.by.site$data<-NULL
+    bio.data$data<-NULL
+    habitat.by.site$data<-NULL
+    site.ID.cols$data<-NULL
+    taxa.ID.cols$data<-NULL
+    abund.ID.cols$data<-NULL
+    habitat.ID.cols$data<-NULL
+    reftest.ID.cols$data<-NULL
+    coord.ID.cols$east<-NULL
+    coord.ID.cols$north<-NULL
+    coord.ID.cols$espg<-NULL
   })
   
   output$rawDataView<-renderDataTable({#Renders raw data table
-    DT::datatable(raw.bio.data(),
-                  options=list(pageLength = 5,scrollX=T))
+    DT::datatable(raw.bio.data$data, options=list(pageLength = 5,scrollX=T))
   })
   
   raw.data.rows<-reactive({
     validate(
-      need(input$rawFormat!="","")
+      need(input$rawFormat!=0,"")
     )
     if (input$rawFormat=="Wide") {
       as.numeric(input$rawData_taxarows)
@@ -40,14 +61,17 @@ shinyServer(function(input, output) {
   
   raw.colnames<-reactive({
     validate(
-      need(input$rawFormat!="","")
+      need(input$rawFormat!=0,"")
     )
-    data<-raw.bio.data()
+    data<-raw.bio.data$data
     output<-as.vector(sapply(data[1:max(raw.data.rows(),1),],paste0,collapse="",sep=";"))
     substr(output,start=1,stop=(nchar(output)-1))
   })
 
   output$wideTaxaCols1 = renderUI({#taxa/metric ID when 2 or more rows used for identifiers - wide format
+    validate(
+      need(input$rawFormat!=0, "Must select Long or Wide data format")
+    )
     selectInput(inputId="widetaxacols1", label=h5('Columns of taxa or metrics'), multiple = TRUE,selectize=FALSE,size=10,
                 choices=raw.colnames()[!raw.colnames()%in%site.ID.cols$data&
                                          !raw.colnames()%in%taxa.ID.cols$data&
@@ -60,9 +84,8 @@ shinyServer(function(input, output) {
                                          ])    
   })
   output$test.vs.ref = renderUI({#taxa/metric ID when 2 or more rows used for identifiers - wide format
-    selectInput(inputId="raw.testrefcols", label=h5('"TEST or REF Site'), multiple = F,selectize=T,selected = "",
-                choices=raw.colnames()[!raw.colnames()%in%site.ID.cols$data&
-                                         !raw.colnames()%in%taxa.ID.cols$data&
+    selectInput(inputId="raw.testrefcols", label=h5('TEST or REF Site'), multiple = F,selectize=T,selected = "",
+                choices=raw.colnames()[!raw.colnames()%in%taxa.ID.cols$data&
                                          !raw.colnames()%in%habitat.ID.cols$data&
                                          !raw.colnames()%in%abund.ID.cols$data&
                                          !raw.colnames()%in%coord.ID.cols$east&
@@ -150,7 +173,7 @@ shinyServer(function(input, output) {
   
   output$finalizeRaw<-reactive({ #when site ID and taxa ID columns are entered, the option to finalize options becomes available
     validate(
-      need(input$rawFormat != "", "")
+      need(input$rawFormat != 0, "")
     )
     if (!is.null(site.ID.cols$data)&!is.null(taxa.ID.cols$data)&input$rawFormat=="Wide"){
       TRUE
@@ -161,79 +184,120 @@ shinyServer(function(input, output) {
       FALSE
       }
     }
-  }) #Detects when enough data have been specified
+  }) #Detects when enough data have been specified to finalize
   outputOptions(output, 'finalizeRaw', suspendWhenHidden=FALSE)
+  
+  output$show_mets<-reactive({
+    validate(
+      need(input$rawFormat != 0, "")
+    )
+    if(!is.null(taxa.by.site$data)) {
+      TRUE
+    } else {
+      FALSE
+    }
+  })  #Detects when enough data have been specified to allow calculation of benthic metrics
+  outputOptions(output, 'show_mets', suspendWhenHidden=FALSE)
+  
+  output$show_mets2<-reactive({
+    validate(
+      need(input$rawFormat != 0, "")
+    )
+    if(!is.null(bio.data$data)) {
+      TRUE
+    } else {
+      FALSE
+    }
+  }) #Detects when enough data have been specified to allow download of benthic metrics
+  outputOptions(output, 'show_mets2', suspendWhenHidden=FALSE)
   
   #########################################################
   #When Raw Data are finalized
   ########################################################
   
-  taxa.by.site<-reactive({
-    validate(
-      need(input$finalize.raw,"Finalize raw data")
-    )
-    input$finalize.raw
+  taxa.by.site<-reactiveValues(data=NULL) #calculate taxa by site table
+  observeEvent(input$finalize_raw,{
     isolate(
       if (input$rawFormat=="Wide"){
-        site.names<-apply(raw.bio.data()[-c(1:max(raw.data.rows(),1)),raw.colnames()%in%site.ID.cols$data],1,paste0,collapse="",sep=";")
+        site.names<-apply(raw.bio.data$data[-c(1:max(raw.data.rows(),1)),raw.colnames()%in%site.ID.cols$data],1,paste0,collapse="",sep=";")
         site.names<-substr(site.names,start=1,stop=(nchar(site.names)-1))
-        output<-raw.bio.data()[max(raw.data.rows()+1,2):nrow(raw.bio.data()),raw.colnames()%in%taxa.ID.cols$data]
-        output<-data.frame(sapply(output,as.numeric))
+        output<-raw.bio.data$data[max(raw.data.rows()+1,2):nrow(raw.bio.data$data),raw.colnames()%in%taxa.ID.cols$data]
+        output<-data.frame(apply(output,2,as.numeric))
         rownames(output)<-site.names
         colnames(output)<-taxa.ID.cols$data
       }
-      )
+    )
     isolate(
       if (input$rawFormat=="Long") {
-        site.names<-sapply(raw.bio.data()[,raw.colnames()%in%site.ID.cols$data],paste0,collapse="",sep=";")
+        site.names<-apply(raw.bio.data$data[-c(1),raw.colnames()%in%site.ID.cols$data],1,paste0,collapse="",sep=";")
         site.names<-substr(site.names,start=1,stop=(nchar(site.names)-1))
         #site.names<-unique(site.names)
         
-        taxa.names<-sapply(raw.bio.data()[,raw.colnames()%in%taxa.ID.cols$data],paste0,collapse="",sep=";")
+        taxa.names<-apply(raw.bio.data$data[-c(1),raw.colnames()%in%taxa.ID.cols$data],1,paste0,collapse="",sep=";")
         taxa.names<-substr(taxa.names,start=1,stop=(nchar(taxa.names)-1))
         #taxa.names<-unique(taxa.names)
         
-        input<-data.frame(sites=site.names,taxa=taxa.names, abund=as.numeric(raw.bio.data()[,raw.colnames()%in%abund.ID.cols$data]))
+        input<-data.frame(sites=site.names,taxa=taxa.names, abund=as.numeric(raw.bio.data$data[-c(1),raw.colnames()%in%abund.ID.cols$data]))
         int.output<-aggregate(abund~sites+taxa, data=input,sum)
         output<-as.data.frame.matrix(xtabs(abund~sites+taxa,data=int.output))
       }
     )
-    output
+    taxa.by.site$data<-output
   })
-  
   output$view.taxa<-renderDataTable({#Renders raw data table
-    DT::datatable(taxa.by.site(),
+    DT::datatable(taxa.by.site$data,
                   options=list(pageLength = 5,scrollX=T))
   })
   
-  
-
+  habitat.by.site<-reactiveValues(data=NULL) #calculate habitat by site table
+  observeEvent(input$finalize_raw,{
+    if (!is.null(habitat.ID.cols$data)){
+      isolate(
+        if (input$rawFormat=="Wide"){
+          site.names<-apply(raw.bio.data$data[-c(1:max(raw.data.rows(),1)),raw.colnames()%in%site.ID.cols$data],1,paste0,collapse="",sep=";")
+          site.names<-substr(site.names,start=1,stop=(nchar(site.names)-1))
+          output<-raw.bio.data$data[max(raw.data.rows()+1,2):nrow(raw.bio.data$data),raw.colnames()%in%habitat.ID.cols$data]
+          output<-data.frame(apply(output,2,as.numeric))
+          rownames(output)<-site.names
+          colnames(output)<-habitat.ID.cols$data
+        }
+      )
+      isolate(
+        if (input$rawFormat=="Long") {
+          site.names<-apply(raw.bio.data$data[,raw.colnames()%in%site.ID.cols$data],1,paste0,collapse="",sep=";")
+          site.names<-substr(site.names,start=1,stop=(nchar(site.names)-1))
+          #site.names<-unique(site.names)
+          
+          output<-data.frame(raw.bio.data$data[,raw.colnames()%in%habitat.ID.cols$data])
+          output<-output[!duplicated(site.names),]
+          rownames(output)<-site.names[!duplicated(site.names)]
+          colnames(output)<-habitat.ID.cols$data
+          output<-output[-c(1),]
+        }
+      )
+      habitat.by.site$data<-output
+    }
+  })
+  output$view.habitat<-renderDataTable({#Renders raw data table
+    DT::datatable(habitat.by.site$data,
+                  options=list(pageLength = 5,scrollX=T))
+  })
+ 
   #########################################################
-  #Input biological Data
+  #Calculate Summary Metrics
   ########################################################
   
-  #bio.data<-reactiveValues()
-  #bio.data<- reactive({
-  #  validate(
-  #    need(input$inbioFile != "", "Please select a data set")
-  #  )
-  #  if (input$metdata==T){
-  #    
-  #  }
-  #  d<-BenthicAnalysistesting::benth.met(x=read.csv(input$inbioFile$datapath, header=F,strip.white=TRUE), tax.fields=input$taxa.names, site.fields=input$site.names, HBI = NULL)
-  #  d
-  #})
-
-  #output$bio.data.view <- renderDataTable({
-  #  bio.data()$Raw.Data
-  #})
+  bio.data<-reactiveValues(data=NULL)
   
-  #output$metric.data.view <- renderDataTable({
-  #  bio.data()$Summary.Metrics
-  #})
+  observeEvent(input$calculate_metrics,{
+    if(input$metdata==F){
+      bio.data$data<-BenthicAnalysistesting::benth.metUI(x=taxa.by.site$data, taxa.sep = input$taxa_sep, HBI=NULL)
+    }
+  })
+  output$view.metrics.raw<-renderDataTable(
+    DT::datatable(bio.data$data$Summary.Metrics, options=list(pageLength = 5,scrollX=T))
+  )
+  output$download_raw_mets<-downloadHandler(filename = function() { paste("Metrics-",input$inrawbioFile, sep='') },
+                  content = function(file) {write.csv(bio.data$data$Summary.Metrics,file)})
   
-  #output$metric.summary.view <- renderPrint({
-  #  summary(bio.data()$Summary.Metrics)
-  #})
-
 })
--- ui.R
+++ ui.R
@@ -1,7 +1,13 @@
+library(shiny)
+library(shinyjs)
+library(shinydashboard)
+library(DT)
+library(leaflet)
+
 ##################################################
 # Header
 ##################################################
-header <- dashboardHeader(
+header <- shinydashboard::dashboardHeader(
   title = "Benthic Analysis Online Tool - Development Version",
   titleWidth = 550,
   dropdownMenu(type = "notifications",
@@ -15,111 +21,173 @@ header <- dashboardHeader(
 ##################################################
 # Sidebar
 ##################################################
-sidebar <- dashboardSidebar(
+sidebar <- shinydashboard::dashboardSidebar(
   sidebarMenu(id = "sidebarmenu",
-    menuItem("Introduction", tabName = "introduction", icon = icon("dashboard")
+              useShinyjs(),
+    menuItem("Introduction", tabName = "introduction", icon = icon("list-alt")
     ),
     menuItem("Data Input", icon = icon("th"), tabName = "datainput",
              menuItem("Raw Data upload",tabName = "rawdatainput",icon=NULL),
             conditionalPanel("input.sidebarmenu === 'rawdatainput'",
+                             useShinyjs(),
                              fileInput(inputId="inrawbioFile",multiple = FALSE, label = h5("Choose CSV file"),accept=c(".csv",".txt")),
                              checkboxInput("metdata",label=h5("Input data are metrics"),value=F),
-                             radioButtons(inputId="rawFormat", label = h5("Input File Format"),choices = list("Long" ,"Wide" ),inline=T, selected = "")
-                             
-            ),
+                             radioButtons(inputId="rawFormat", label = h5("Input File Format"),choices = list("None Selected"=0,"Long"="Long" ,"Wide"="Wide" ),inline=F, selected = 0)
+                             ),
             conditionalPanel("output.finalizeRaw==true && input.sidebarmenu === 'rawdatainput'",
-                             actionButton("finalize.raw","Finalize")
-                             )
-    )
+                             actionButton("finalize_raw","Finalize"),
+                             actionButton("clear_all","Clear")
+                             ),
+            menuItem("Metric Transformations",tabName = "transSummaryMetrics",icon=NULL),
+            menuItem("User Matched Reference Sites",tabName = "userMatchRef",icon=NULL),
+            menuItem("Mapping",tabName = "Mapping",icon=NULL)
+    ),
+    menuItem("RCA",tabname="RCA",icon=icon("tint", lib = "glyphicon")),
+    menuItem("Trends",tabname="Trends",icon=icon("tree-deciduous", lib = "glyphicon"))
   )
 )
 
 ##################################################
 # Body
 ##################################################
-body <- dashboardBody(
+body <- shinydashboard::dashboardBody(
   tabItems(
     ##################################################
     # Intruduction Page
     ##################################################
     
     tabItem(tabName = "introduction",
-            h2("Introduction"),
-            fluidRow(
-              helpText("Hold")
-            )
+            box(title="Introduction",width=12,status="info",collapsible = T, collapsed = F,solidHeader = T,
+                h3("UNDER HEAVY DEVELOPMENT"),
+                helpText("Online tool for analysis of aquatic biomonitoring data")
+                ),
+            box(title="Implemented features",width=12,status="info",collapsible = T, collapsed = T,solidHeader = T,
+                helpText("- Single input file for the follow data types: taxa or metrics, habitat descriptors, coordinates of sampling point"),
+                helpText("- Support for long and wide data formats (long format recommended for lowest-practical-level taxonomy)"),
+                helpText("- Support for wide data formats with multiple column headers"),
+                helpText("- Calculation of indicator metrics for Benthic Macroinvertebrate taxa"),
+                helpText("- HBI will be calculated at family level only - for now"),
+                helpText("- Functional metrics can be calculated below family level"),
+                helpText("- Download calculated Summary metrics as .csv file")
+                ),
+            box(title="Planned features",width=12,status="info",collapsible = T, collapsed = T,solidHeader = T,
+                helpText("- Better support for lowest-practical-level taxonomy"),
+                helpText("- GIS intigration for mapping of test and reference sites"),
+                helpText("- GIS intigration for calculation of catchment and site level attributes"),
+                helpText("- Reference Condition Approach Bioassessment using Test Site Analysis and Assessment by Nearest-Neighbour Analysis"),
+                helpText("- Trend analysis using Generalized Linear Mixed-Effects models")
+                ),
+            box(title="Known Bugs",width=12,status="info",collapsible = T, collapsed = T,solidHeader = T,
+                helpText("- none")
+                )
     ),
     ##################################################
     # Raw Data Input
     ##################################################
     
     tabItem(tabName = "rawdatainput",
+            useShinyjs(),
+            
             h2("Raw Data Input"),
             fluidRow(
               tabBox("Data",width=12,
                 tabPanel("Raw",status="warning",collapsible = T,solidHeader = T,
-                         DT::dataTableOutput("rawDataView")
+                         fluidRow(
+                           DT::dataTableOutput("rawDataView"),
+                           br(),
+                           br()
+                         ),
+                         fluidRow(
+                           column(width=6,
+                                  box(title="Columns",width=12,status="primary",collapsible = T,solidHeader = T,collapsed = T,
+                                      helpText("Highlight columns (multiple with shift) then assign them to an attribute."),
+                                      conditionalPanel(condition="input.rawFormat == 'Wide'",
+                                                       numericInput("rawData_taxarows", label = h5("Rows of column Identifiers"), min=1,value=1)
+                                      ),
+                                      conditionalPanel("input.rawFormat == 'Wide' && input.rawData_taxarows == 1 ",
+                                                       textInput("taxa_sep", label = h5("Character that separates taxa names"), value = ";")
+                                      ),
+                                      uiOutput("wideTaxaCols1")
+                                  )
+                                  
+                           ),
+                           column(width=6,
+                                  conditionalPanel("input.rawFormat == 'Wide'||input.rawFormat == 'Long'",
+                                                   box(title="Assign Columns to Attributes", width=NULL,status="success",collapsible = T,solidHeader = T,collapsed = T,                                                       column(width=6,
+                                                              actionButton("raw.siteID.cols", "Site/Sampling Events"),
+                                                              br(),
+                                                              actionButton("raw.taxa.cols", "Taxa or Metrics"),
+                                                              br(),
+                                                              actionButton("raw.habitat.cols", "Habitat Descriptors (Optional)"),
+                                                              br(),
+                                                              conditionalPanel("input.rawFormat == 'Long'",actionButton("raw.abund.cols", "Abundances"))
+                                                       ),
+                                                       column(width=6,
+                                                              actionButton("raw.siteID.cols.rem", "Undo"),
+                                                              br(),
+                                                              actionButton("raw.taxa.cols.rem", "Undo"),
+                                                              br(),
+                                                              actionButton("raw.habitat.cols.rem", "Undo"),
+                                                              br(),
+                                                              conditionalPanel("input.rawFormat == 'Long'",actionButton("raw.abund.cols.rem", "Undo"))
+                                                       )
+                                      )
+                                  ),
+                                  conditionalPanel("input.rawFormat == 'Wide'||input.rawFormat == 'Long'",
+                                                   box(title="Test vs. Reference (Optional)",width=NULL,status="success",collapsible = T,solidHeader = T,collapsed = T,
+                                                       uiOutput("test.vs.ref"),
+                                                       actionButton("raw.testref.cols", "Test and Reference Sites"),
+                                                       actionButton("raw.testref.cols.rem", "Undo")
+                                                   ),
+                                                   box(title="Coordinates (Optional)",width=NULL,status="success",collapsible = T,solidHeader = T,collapsed = T,
+                                                       uiOutput("eastingCols"),
+                                                       uiOutput("northingCols"),
+                                                       uiOutput("ESPGCols"),
+                                                       actionButton("raw.coord.cols", "Coordinates"),
+                                                       actionButton("raw.coord.cols.rem", "Undo")
+                                                   )
+                                  )
+                           )
+                         )
+                         
                          ),
                 tabPanel("Taxa",
-                         DT::dataTableOutput("view.taxa")
+                         useShinyjs(),
+                         
+                         DT::dataTableOutput("view.taxa"),
+                         br(),
+                         br(),
+                         conditionalPanel(condition="input.metdata == false && input.finalize_raw>0 && output.show_mets == true",
+                                          fluidRow(
+                                            column(width=6,
+                                                   box(title="Calculate Summary Metrics",width=12,status="primary",collapsible = T,solidHeader = T,collapsed = F,
+                                                       actionButton("calculate_metrics","Calculate Summary Metrics"),
+                                                       conditionalPanel(condition="input.metdata == false && input.calculate_metrics>0 && output.show_mets2 == true",
+                                                                        downloadButton("download_raw_mets","Download Summary Metrics")
+                                                                        )
+                                                       )
+                                                   )
+                                            )
+                                          ),
+                         conditionalPanel(condition="input.metdata == false && input.calculate_metrics>0 && output.show_mets == true",
+                                          fluidRow(
+                                            column(width=12,
+                                                   box(title="Summary Metrics",width=12,status="primary",collapsible = T,solidHeader = T,collapsed = T,
+                                                       DT::dataTableOutput("view.metrics.raw")
+                                                   )
+                                            )
+                                          )
+                         )
                          ),
-                tabPanel("Habitat")
-              )
-            ),
-            fluidRow(
-              column(width=6,
-                box(title="Columns",width=12,status="primary",collapsible = T,solidHeader = T,
-                    conditionalPanel(condition="input.rawFormat == 'Wide'",
-                                     numericInput("rawData_taxarows", label = h5("Rows of column Identifiers"), min=1,value=1)
-                    ),
-                    conditionalPanel("input.rawFormat == 'Wide' && input.rawData_taxarows == 1 ",
-                                     textInput("text", label = h5("Character that separates taxa names"), value = ";")
-                    ),
-                    uiOutput("wideTaxaCols1")
-                )
-                
-              ),
-              column(width=6,
-                box(title="Assign Columns", width=NULL,status="success",collapsible = T,solidHeader = T,
-                    conditionalPanel("input.rawFormat == 'Wide'||input.rawFormat == 'Long'",
-                                     column(width=6,
-                                            actionButton("raw.siteID.cols", "Site/Sampling Events"),
-                                            br(),
-                                            actionButton("raw.taxa.cols", "Taxa or Metrics"),
-                                            br(),
-                                            actionButton("raw.habitat.cols", "Habitat Descriptors"),
-                                            br(),
-                                            conditionalPanel("input.rawFormat == 'Long'",actionButton("raw.abund.cols", "Values"))
-                                     ),
-                                     column(width=6,
-                                            actionButton("raw.siteID.cols.rem", "Undo"),
-                                            br(),
-                                            actionButton("raw.taxa.cols.rem", "Undo"),
-                                            br(),
-                                            actionButton("raw.habitat.cols.rem", "Undo"),
-                                            br(),
-                                            conditionalPanel("input.rawFormat == 'Long'",actionButton("raw.abund.cols.rem", "Undo"))
-                                            
-                                     )
-                    )
-                ),
-                conditionalPanel("input.rawFormat == 'Wide'||input.rawFormat == 'Long'",
-                                 box(title="Test vs. Reference",width=NULL,status="success",collapsible = T,solidHeader = T,collapsed = T,
-                                     uiOutput("test.vs.ref"),
-                                     actionButton("raw.testref.cols", "Test and Reference Sites"),
-                                     actionButton("raw.testref.cols.rem", "Undo")
-                                     ),
-                                 box(title="Coordinates",width=NULL,status="success",collapsible = T,solidHeader = T,collapsed = T,
-                                     uiOutput("eastingCols"),
-                                     uiOutput("northingCols"),
-                                     uiOutput("ESPGCols"),
-                                     actionButton("raw.coord.cols", "Coordinates"),
-                                     actionButton("raw.coord.cols.rem", "Undo")
-                                     )
-                )
-              )
+                tabPanel("Habitat",
+                         DT::dataTableOutput("view.habitat"),
+                         br(),
+                         br()
+                         )
               )
             )
+            )#,
+    #tabItem(tabname="summary.metrics")
     )
   )
 
